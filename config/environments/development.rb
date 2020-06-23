cc4c4c6ad732a12159501b021041573033c021ed
Vera-cpu <vg.gurskaya@gmail.com>
2020-06-23 01:36:14 +0000

    devise_for_authorization

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.hosts << "a9e0b8dd3dd644eca86dbfb289ad237c.vfs.cloud9.ca-central-1.amazonaws.com"
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
